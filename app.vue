a2ec7a81c46882ef9bf0fb89296072c643227e86
mlengse <anjangkn@gmail.com>
2023-05-18 18:22:06 +0700

    kj

--- app.vue
+++ app.vue
@@ -6,3 +6,13 @@
     <NuxtPage />
   </AppLayout>
 </template>
+<script setup lang="ts">
+// a. Composition API
+defineOgImageScreenshot({
+  // wait for animations
+  // delay: 1000,
+  width: 712,
+  colorScheme: 'dark',
+  seletor: '.page-body'
+})
+</script>
