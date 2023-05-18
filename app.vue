2f87b8d274fe366a47b98766525374d6396cb1d3
mlengse <anjangkn@gmail.com>
2023-05-18 18:52:42 +0700

    tes 4

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
     <SeoKit />
     <!-- a. Generates browser screenshots for every page -->
     <OgImageScreenshot 
-      width="712"
+      :width='712'
       colorScheme="dark"
       selector=".page-body"
     />
