ce8a6a2e8126335f5a526b967892f15b61f12fe3
mlengse <anjangkn@gmail.com>
2023-05-22 23:31:01 +0700

    2.1.1

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,31 @@
 export default defineAppConfig({
+  // the alternative to Nuxt theme https://nuxt-themes.netlify.app/
+  bloginote: {
+    // author: 'Benbinbin',
+    // email: 'benthomsonbin@gmail.com',
+    // avatar: '/default-avatar.png',
+    // favicon: '/default-favicon.ico',
+    // scrollSmooth: true,
+    // homePage: {
+    //   showBlogPosts: true,
+    //   postItemLimit: 20
+    // },
+    // subscribePage: true,
+    // articlePage: {
+    //   showTime: true,
+    //   outdated: {
+    //     show: true,
+    //     threshold: 30
+    //   },
+    //   showCatalog: true,
+    // },
+    search: {
+      exclude: [
+        '/',
+      ]
+    },
+    assetTypes: []
+  },
   docus: {
     title: 'Akreditasi 2023',
     description: 'Puskesmas Jayengan. Sahabat menuju sehat.',
---
+++ components/PageSearch.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+const showSearchModal = useState('showSearchModal')
+</script>
+
+<template>
+  <div
+    class="doc-search"
+    @click="showSearchModal=true"
+  >
+    <button
+      type="button"
+      aria-label="Cari"
+    >
+      <span class="content">
+        <Icon name="heroicons-outline:search" />
+        <span>
+          <kbd>Ctrl</kbd>
+          <kbd>+</kbd>
+          <kbd>K</kbd>
+        </span>
+      </span>
+    </button>
+  </div>
+</template>
+
+<style scoped lang="ts">
+css({
+  '.doc-search': {
+    '&:hover': {
+      button: {
+        borderColor: '{color.gray.300}'
+      }
+    },
+    button: {
+      padding: '{space.2} {space.4}',
+      '.content': {
+        borderRadius: '{radii.md}',
+        display: 'flex',
+        alignItems: 'center',
+        color: '{color.gray.500}',
+        borderStyle: 'solid',
+        borderWidth: '1px',
+        borderColor: '{color.gray.100}',
+        fontSize: '{fontSize.xs}',
+        gap: '{space.2}',
+        padding: '{space.rem.375}',
+        '@dark': {
+          color: '{color.gray.400}',
+          borderColor: '{color.gray.900}',
+        },
+        '&:hover': {
+          color: '{color.gray.700}',
+          borderColor: '{color.gray.400}',
+          '@dark': {
+            color: '{color.gray.200}',
+            borderColor: '{color.gray.700}',
+          }
+        },
+        span: {
+          '&:first-child': {
+            display: 'block',
+            fontSize: '{fontSize.xs}',
+            fontWeight: '{fontWeight.medium}',
+          },
+          '&:nth-child(2)': {
+            flex: 'none',
+            display: 'none',
+            fontSize: '{fontSize.xs}',
+            fontWeight: '{fontWeight.semibold}',
+            '@sm': {
+              display: 'block'
+            }
+          }
+        }
+      }
+    },
+
+  }
+})
+</style>
