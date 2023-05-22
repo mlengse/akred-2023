00b2ee45a670d3f07ee5cbfb218469c5cc5ec618
mlengse <medtosys@gmail.com>
2023-05-22 15:03:04 +0700

    1.5.1 s.d. 2.1.2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,9 +13,9 @@ export default defineNuxtConfig({
         applicationId: process.env.ALGOLIA_APPLICATION_ID,
         apiKey: process.env.ALGOLIA_API_KEY,
         langAttribute: "lang",
-        docSearch: {
-          indexName: process.env.ALGOLIA_INDEX_NAME,
-        }
+        // docSearch: {
+        //   indexName: process.env.ALGOLIA_INDEX_NAME,
+        // },
       }
     }
   },
@@ -32,61 +32,61 @@ export default defineNuxtConfig({
   //   docSearch:{
   //     indexName: process.env.ALGOLIA_INDEX_NAME,
   //   },
-  //   // crawler: {
-  //   //   apiKey: process.env.ALGOLIA_CRAWLER_KEY,
-  //   //   indexName: process.env.ALGOLIA_INDEX_NAME,
-  //   //   // meta: ['title', 'description'],
-  //   //   // meta: async (html: string, route: string) => {
-  //   //   //   const isInclude = !route.startsWith('admin') 
-  //   //   //     && !route.endsWith('.png') 
-  //   //   //     && !route.endsWith('.js')
-  //   //   //     && !route.endsWith('.json')
-  //   //   //     && !route.endsWith('.txt')
+  //   crawler: {
+  //     apiKey: process.env.ALGOLIA_CRAWLER_KEY,
+  //     indexName: process.env.ALGOLIA_INDEX_NAME,
+  //     meta: ['title', 'description'],
+  //     // meta: async (html: string, route: string) => {
+  //     //   const isInclude = !route.startsWith('admin') 
+  //     //     && !route.endsWith('.png') 
+  //     //     && !route.endsWith('.js')
+  //     //     && !route.endsWith('.json')
+  //     //     && !route.endsWith('.txt')
 
-  //   //   //   console.log(html)
-  //   //   //   return ['title', 'description']
-  //   //   //   // const result = await somePromise(route)
-  //   //   //   // return {
-  //   //   //   //   ...result,
-  //   //   //   //   foo: 'bar'
-  //   //   //   // }
-  //   //   // },
-  //   //   include:   (route: string) => {
-  //   //     const isInclude = !route.startsWith('admin') 
-  //   //       && !route.endsWith('.png') 
-  //   //       && !route.endsWith('.js')
-  //   //       && !route.endsWith('.json')
-  //   //       && !route.endsWith('.txt')
+  //     //   console.log(html)
+  //     //   return ['title', 'description']
+  //     //   // const result = await somePromise(route)
+  //     //   // return {
+  //     //   //   ...result,
+  //     //   //   foo: 'bar'
+  //     //   // }
+  //     // },
+  //     include:   (route: string) => {
+  //       const isInclude = !route.startsWith('admin') 
+  //         && !route.endsWith('.png') 
+  //         && !route.endsWith('.js')
+  //         && !route.endsWith('.json')
+  //         && !route.endsWith('.txt')
 
-  //   //     // console.log(isInclude)
-  //   //     return isInclude
-  //   //   },
-  //   //   // "index_name": process.env.ALGOLIA_INDEX_NAME,
-  //   //   // // "start_urls": ["*******"],
-  //   //   // // "stop_urls": ["*******"],
-  //   //   // "selectors": {
-  //   //   //   "lvl0": {
-  //   //   //     "selector": "",
-  //   //   //     "default_value": ""
-  //   //   //   },
-  //   //   //   "lvl1": "article h1",
-  //   //   //   "lvl2": "article h2",
-  //   //   //   "lvl3": "article h3",
-  //   //   //   "lvl4": "article h4",
-  //   //   //   "lvl5": "article h5",
-  //   //   //   "text": "article p, article li",
-  //   //   //   "lang": {
-  //   //   //     "selector": "/html/@lang",
-  //   //   //     "type": "xpath",
-  //   //   //     "global": true
-  //   //   //   }
-  //   //   // },
-  //   //   // "strip_chars": " .,;:#",
-  //   //   // "selectors_exclude": [".sr-only"],
-  //   //   // "custom_settings": {
-  //   //   //   "attributesForFaceting": ["lang", "tags"]
-  //   //   // }
-  //   // },
-  //   // recommend: true,
+  //       // console.log(isInclude)
+  //       return isInclude
+  //     },
+  //     // "index_name": process.env.ALGOLIA_INDEX_NAME,
+  //     // // "start_urls": ["*******"],
+  //     // // "stop_urls": ["*******"],
+  //     // "selectors": {
+  //     //   "lvl0": {
+  //     //     "selector": "",
+  //     //     "default_value": ""
+  //     //   },
+  //     //   "lvl1": "article h1",
+  //     //   "lvl2": "article h2",
+  //     //   "lvl3": "article h3",
+  //     //   "lvl4": "article h4",
+  //     //   "lvl5": "article h5",
+  //     //   "text": "article p, article li",
+  //     //   "lang": {
+  //     //     "selector": "/html/@lang",
+  //     //     "type": "xpath",
+  //     //     "global": true
+  //     //   }
+  //     // },
+  //     // "strip_chars": " .,;:#",
+  //     // "selectors_exclude": [".sr-only"],
+  //     // "custom_settings": {
+  //     //   "attributesForFaceting": ["lang", "tags"]
+  //     // }
+  //   },
+  //   recommend: true,
   // }]],
 })
